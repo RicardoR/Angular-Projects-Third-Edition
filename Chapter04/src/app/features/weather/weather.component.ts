7bbb9bf7511ed223dee0f1017d850a90f5f67565
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-02-15 17:51:46 +0100

    feat: display weather data

--- Chapter04/src/app/features/weather/weather.component.ts
+++ Chapter04/src/app/features/weather/weather.component.ts
@@ -1,21 +1,44 @@
-import { Component, inject, Injector, Signal } from '@angular/core';
+import { Component, inject, Injector, signal, Signal } from '@angular/core';
 import { Weather } from '../weather';
 import { WeatherService } from './weather.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { MatFormField } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardSubtitle,
+  MatCardTitle
+} from '@angular/material/card';
+import { DecimalPipe } from '@angular/common';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormField,
+    MatIcon,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardImage,
+    DecimalPipe,
+    MatInput
+  ],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
 })
 export class WeatherComponent {
   private injector = inject(Injector)
-  weather!: Signal<Weather|undefined>;
+  weather: Signal<Weather> = signal({} as Weather);
   private weatherService = inject(WeatherService);
   
   search(city: string): void {
-    this.weather = toSignal(this.weatherService.getWeather(city), {injector: this.injector});
+    this.weather = toSignal(this.weatherService.getWeather(city), {injector: this.injector, initialValue: {} as Weather});
   }
 }
