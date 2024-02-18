742cfd6ab70711f981a14d36933599198b8d75e1
Ricardo <ricardo.rv@outlook.com>
2024-02-18 17:58:41 +0100

    feat: take a photo

--- Chapter06/src/app/capture/capture.page.ts
+++ Chapter06/src/app/capture/capture.page.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { addIcons } from 'ionicons';
+import { PhotoService } from '../photo.service';
 
 @Component({
   selector: 'app-capture',
   templateUrl: './capture.page.html',
   styleUrls: ['./capture.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class CapturePage {
+  private photoService = inject(PhotoService);
 
-
+  openCamera(): void {
+    this.photoService.takePhoto();
+  }
 }
---
+++ Chapter06/src/app/photo.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation } from '@capacitor/geolocation';
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  private async getLocation() {
+    const location = await Geolocation.getCurrentPosition();
+    return location.coords;
+  }
+
+  async takePhoto() {
+    await this.getLocation();
+    await Camera.getPhoto({
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+      quality: 100
+    })
+  }
+}
