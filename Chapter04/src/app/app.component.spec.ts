43da287126fc0234ab6c8ffb2f4e8b264728522d
Ricardo <ricardo.rv@outlook.com>
2024-02-18 11:10:20 +0100

    feat: reload page when new version is available

--- Chapter04/src/app/app.component.spec.ts
+++ Chapter04/src/app/app.component.spec.ts
@@ -1,18 +1,66 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  SwUpdate,
+  VersionReadyEvent,
+} from '@angular/service-worker';
 import { AppComponent } from './app.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentFixture, TestBed, fakeAsync, flush } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { of } from 'rxjs';
+import { ReloadService } from './reload.service';
 
 describe('AppComponent', () => {
+  let swUpdateSpy: jasmine.SpyObj<SwUpdate>;
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let mockSwUpdate: Partial<SwUpdate>;
+  let mockSnackbar: Partial<MatSnackBar>;
+  let reloadService: jasmine.SpyObj<ReloadService>;
+
+
   beforeEach(async () => {
+    let reloadSpyObj = jasmine.createSpyObj('ReloadService', ['reloadPage']);
+
+    mockSwUpdate = {
+      versionUpdates: of({ type: 'VERSION_READY' } as VersionReadyEvent),
+      activateUpdate: jasmine.createSpy('activateUpdate').and.resolveTo(true),
+    };
+    mockSnackbar = {
+      open: jasmine.createSpy('open').and.returnValue({
+        afterDismissed: () => of({ dismissedByAction: true }),
+      }),
+    };
+
     await TestBed.configureTestingModule({
       imports: [AppComponent, HttpClientTestingModule, BrowserAnimationsModule],
+      providers: [
+        { provide: SwUpdate, useValue: mockSwUpdate },
+        { provide: MatSnackBar, useValue: mockSnackbar },
+        { provide: ReloadService, useValue: reloadSpyObj }
+      ],
     }).compileComponents();
+
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      reloadService = TestBed.inject(ReloadService) as jasmine.SpyObj<ReloadService>;
+      reloadService.reloadPage.and.returnValue();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
+   it('should subscribe to versionUpdates and perform update when a new version is available', fakeAsync(() => {
+
+     component.ngOnInit();
+     flush();
+     expect(mockSnackbar.open).toHaveBeenCalledWith(
+       'A new version is available!',
+       'Update now'
+     );
+     expect(mockSwUpdate.activateUpdate).toHaveBeenCalled();
+     expect(reloadService.reloadPage).toHaveBeenCalled();
+   }));
 });
--- Chapter04/src/app/app.component.ts
+++ Chapter04/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Component, OnInit, inject, DestroyRef } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, switchMap, map } from 'rxjs';
+import { ReloadService } from './reload.service';
 import { WeatherComponent } from './features/weather/weather.component';
 
 @Component({
@@ -10,6 +15,19 @@ import { WeatherComponent } from './features/weather/weather.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit {
+  private updates = inject(SwUpdate);
+  private snackbar = inject(MatSnackBar);
+  private destroyRef = inject(DestroyRef);
+  private reloadService = inject(ReloadService)
+
+  ngOnInit(): void {
+    this.updates.versionUpdates.pipe(
+      takeUntilDestroyed(this.destroyRef),
+      filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+      switchMap(() => this.snackbar.open('A new version is available!', 'Update now').afterDismissed()),
+      filter(result => result.dismissedByAction),
+      map(() => this.updates.activateUpdate().then(() => this.reloadService.reloadPage()))
+    ).subscribe()
+  }
 }
