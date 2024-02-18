0c84bb55a3dc07be1dd975da8a2dc7e44e87f1f0
Ricardo <ricardo.rv@outlook.com>
2024-02-18 17:56:52 +0100

    test: fix app component ut

--- Chapter06/src/app/app.component.spec.ts
+++ Chapter06/src/app/app.component.spec.ts
@@ -23,9 +23,9 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-label');
-    expect(menuItems.length).toEqual(12);
-    expect(menuItems[0].textContent).toContain('Inbox');
-    expect(menuItems[1].textContent).toContain('Outbox');
+    expect(menuItems.length).toEqual(2);
+    expect(menuItems[0].textContent).toContain('Take a photo');
+    expect(menuItems[1].textContent).toContain('View gallery');
   });
 
   it('should have urls', () => {
@@ -33,12 +33,12 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-item');
-    expect(menuItems.length).toEqual(12);
+    expect(menuItems.length).toEqual(2);
     expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual(
-      '/folder/inbox'
+      '/capture'
     );
     expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual(
-      '/folder/outbox'
+      '/view'
     );
   });
 });
