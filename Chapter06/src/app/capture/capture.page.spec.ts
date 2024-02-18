61d7ff26cac7901eabdbab46f071127779bec109
Ricardo <ricardo.rv@outlook.com>
2024-02-18 17:57:12 +0100

    ut: add a test for taking a picture

--- Chapter06/src/app/capture/capture.page.spec.ts
+++ Chapter06/src/app/capture/capture.page.spec.ts
@@ -1,17 +1,33 @@
+import { PhotoService } from './../photo.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CapturePage } from './capture.page';
 
 describe('CapturePage', () => {
   let component: CapturePage;
   let fixture: ComponentFixture<CapturePage>;
+  let photoServiceSpy: jasmine.SpyObj<PhotoService>;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
+    let photoServiceSpyObj = jasmine.createSpyObj('PhotoService', ['takePhoto']);
+
+    await TestBed.configureTestingModule({
+      imports: [CapturePage],
+      providers: [{ provide: PhotoService, useValue: photoServiceSpyObj }]
+    }).compileComponents()
+
     fixture = TestBed.createComponent(CapturePage);
     component = fixture.componentInstance;
+    photoServiceSpy = TestBed.inject(PhotoService) as jasmine.SpyObj<PhotoService>;
+
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should take a photo when openCamera is called', () => {
+    component.openCamera();
+    expect(photoServiceSpy.takePhoto).toHaveBeenCalled();
+  })
 });
