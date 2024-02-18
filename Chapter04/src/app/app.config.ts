036fa33f5612d995ecb49aa85ee956cf165b73aa
Ricardo <ricardo.rv@outlook.com>
2024-02-18 10:07:32 +0100

    chore: add and configure pwapp

--- Chapter04/src/app/app.config.ts
+++ Chapter04/src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-      provideAnimationsAsync(),
-      provideHttpClient()
-  ]
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
