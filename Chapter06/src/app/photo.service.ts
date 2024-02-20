b42490d5c6e297cb3a3a7994f8ad0af9f0722f7f
Ricardo <ricardo.rv@outlook.com>
2024-02-20 12:37:20 +0100

    feat: add photoservice

--- Chapter06/src/app/photo.service.ts
+++ Chapter06/src/app/photo.service.ts
@@ -1,22 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Geolocation } from '@capacitor/geolocation';
+
+export interface SavePhotoResult {
+  name: string;
+  lat: number;
+  long: number;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotoService {
-
   private async getLocation() {
     const location = await Geolocation.getCurrentPosition();
     return location.coords;
   }
 
   async takePhoto() {
-    await this.getLocation();
-    await Camera.getPhoto({
-      resultType: CameraResultType.DataUrl,
-      source: CameraSource.Camera,
-      quality: 100
-    })
+   const { latitude, longitude } = await this.getLocation();
+   const cameraPhoto = await Camera.getPhoto({
+     resultType: CameraResultType.DataUrl,
+     source: CameraSource.Camera,
+     quality: 100,
+   });
+
+    if (cameraPhoto.dataUrl) {
+      await this.savePhoto(cameraPhoto.dataUrl, latitude, longitude);
+    }
+  }
+
+  private async savePhoto(
+    dataUrl: string,
+    latitude: number,
+    longitude: number
+  ): Promise<{ name: string; lat: number; long: number }> {
+    const name = new Date().getUTCMilliseconds().toString();
+    const promiseToReturn = new Promise<SavePhotoResult>((resolve) => {
+      setTimeout(() => {
+        resolve({ name, lat: latitude, long: longitude });
+      }, 1000);
+    });
+
+    return promiseToReturn;
   }
 }
