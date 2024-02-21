9c264623439e7cf7360df18ad3d2d882e56e180e
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-02-21 19:13:41 +0100

    :test_tube: ut: check if template contains the app-personal-info

--- Chapter07/src/app/app.component.spec.ts
+++ Chapter07/src/app/app.component.spec.ts
@@ -1,29 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have the username value`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.username).toEqual('ricardo-roguez');
   });
 
   it('should render the link to the repo', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('a')?.getAttribute('href')).toBe('https://github.com/ricardo-roguez');
+  });
+
+  it('should contain the personal info', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-personal-info')).toBeTruthy();
   });
 });
