8b45ff8a5641dd48c7635ab47a4b12f764e1dbf5
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-02-22 18:43:04 +0100

    rollback ngsrc

--- Chapter07/src/app/features/personal-info/personal-info.component.ts
+++ Chapter07/src/app/features/personal-info/personal-info.component.ts
@@ -2,14 +2,11 @@ import { Component, inject } from '@angular/core';
 import { GithubService } from '../../shared/api/github.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { User } from '../../shared/domain/user';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-personal-info',
   standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
+  imports: [],
   templateUrl: './personal-info.component.html',
   styleUrl: './personal-info.component.scss'
 })
