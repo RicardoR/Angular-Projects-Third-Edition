4acabb5af8f5aec234bc25413fe8bdac3037a4b3
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-02-22 18:31:51 +0100

    ref: improve src attr

--- Chapter07/src/app/features/personal-info/personal-info.component.ts
+++ Chapter07/src/app/features/personal-info/personal-info.component.ts
@@ -2,11 +2,14 @@ import { Component, inject } from '@angular/core';
 import { GithubService } from '../../shared/api/github.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { User } from '../../shared/domain/user';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-personal-info',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './personal-info.component.html',
   styleUrl: './personal-info.component.scss'
 })
