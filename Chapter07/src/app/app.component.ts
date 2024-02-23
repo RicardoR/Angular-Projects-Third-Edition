ac807c6250402175de2fc4d2a69d47c0916879c2
Ricardo <ricardo.rv@outlook.com>
2024-02-23 18:54:01 +0100

    feat: add title and meta data

--- Chapter07/src/app/app.component.ts
+++ Chapter07/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { PersonalInfoComponent } from './features/personal-info/personal-info.component';
 import { RepositoriesComponent } from "./features/repositories/repositories.component";
 import { OrganizationsComponent } from "./features/organizations/organizations.component";
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-root',
@@ -10,12 +11,29 @@ import { OrganizationsComponent } from "./features/organizations/organizations.c
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     imports: [
-        NgOptimizedImage,
-        PersonalInfoComponent,
-        RepositoriesComponent,
-        OrganizationsComponent
+      NgOptimizedImage,
+      PersonalInfoComponent,
+      RepositoriesComponent,
+      OrganizationsComponent
     ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  private titleService = inject(Title);
+  private meta = inject(Meta);
+
   username = 'ricardo-roguez';
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Github portfolio app');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${this.username}'s GitHub portfolio`,
+      },
+      {
+        name: 'author',
+        content: this.username,
+      },
+    ]);
+  }
 }
