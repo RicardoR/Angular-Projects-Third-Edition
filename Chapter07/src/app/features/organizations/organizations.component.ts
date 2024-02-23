a59cce60529f5877bf8cb9e75a6f4b255c59b141
Ricardo <ricardo.rv@outlook.com>
2024-02-23 18:35:02 +0100

    feat: display organization list in the view

--- Chapter07/src/app/features/organizations/organizations.component.ts
+++ Chapter07/src/app/features/organizations/organizations.component.ts
@@ -2,13 +2,14 @@ import { Component, Signal, inject } from '@angular/core';
 import { Organization } from '../../shared/domain/organization';
 import { GithubService } from '../../shared/api/github.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { PanelComponent } from "../panel/panel.component";
 
 @Component({
-  selector: 'app-organizations',
-  standalone: true,
-  imports: [],
-  templateUrl: './organizations.component.html',
-  styleUrl: './organizations.component.scss'
+    selector: 'app-organizations',
+    standalone: true,
+    templateUrl: './organizations.component.html',
+    styleUrl: './organizations.component.scss',
+    imports: [PanelComponent]
 })
 export class OrganizationsComponent {
   private githubService = inject(GithubService);
