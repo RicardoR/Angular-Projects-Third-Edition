f4c2227c69203cf7b82adf1be2380bef0e8f5155
Ricardo <ricardo.rv@outlook.com>
2024-02-23 17:59:57 +0100

    feat: display non forked repositories

--- Chapter07/src/app/app.component.ts
+++ Chapter07/src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { PersonalInfoComponent } from './features/personal-info/personal-info.component';
+import { RepositoriesComponent } from "./features/repositories/repositories.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    NgOptimizedImage,
-    PersonalInfoComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [
+        NgOptimizedImage,
+        PersonalInfoComponent,
+        RepositoriesComponent
+    ]
 })
 export class AppComponent {
   username = 'ricardo-roguez';
