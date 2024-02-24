01024664b81f7744d84d662674a842aeb4d8dc3a
Ricardo <ricardo.rv@outlook.com>
2024-02-24 20:33:52 +0100

    ref

--- Chapter08/apps/tour/src/app/app.routes.ts
+++ Chapter08/apps/tour/src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { VisitorComponent } from '@packt/visitor';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'tour',
+    loadComponent: () =>
+      import('@packt/visitor').then((c) => c.VisitorComponent),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'tour',
+  },
+];
--- Chapter08/libs/src/lib/visitor/visitor.component.spec.ts
+++ Chapter08/libs/src/lib/visitor/visitor.component.spec.ts
@@ -1,21 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { VisitorComponent } from './visitor.component';
 
 describe('VisitorComponent', () => {
   let component: VisitorComponent;
   let fixture: ComponentFixture<VisitorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [VisitorComponent],
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule]
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(VisitorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should compile', () => {
     expect(component).toBeTruthy();
   });
 });
