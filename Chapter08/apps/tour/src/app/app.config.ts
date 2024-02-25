d4b488c023180ab7938784c8c46b4e71ae63eff4
Ricardo <ricardo.rv@outlook.com>
2024-02-25 12:08:43 +0100

    chore: add store

--- Chapter08/apps/tour/src/app/app.config.ts
+++ Chapter08/apps/tour/src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideAnimationsAsync()],
+  providers: [
+    provideEffects(),
+    provideStore(),
+    provideEffects(),
+    provideStore(),
+    provideRouter(appRoutes),
+    provideAnimationsAsync(),
+  ],
 };
