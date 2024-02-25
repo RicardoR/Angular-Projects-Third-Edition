e398a1cd53519cb090eb04fca53063fca1ff7aa4
Ricardo <ricardo.rv@outlook.com>
2024-02-25 12:35:03 +0100

    chore: add poi store in library

---
+++ Chapter08/libs/poi/src/poi.routes.ts
@@ -0,0 +1,17 @@
+import { Route } from '@angular/router';
+import { PoiComponent } from './lib/poi/poi.component';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import * as fromPoi from './state/poi.reducer';
+import { PoiEffects } from './state/poi.effects';
+
+export const poiRoutes: Route[] = [
+  {
+    path: '',
+    component: PoiComponent,
+    providers: [
+      provideState(fromPoi.POI_FEATURE_KEY, fromPoi.poiReducer),
+      provideEffects(PoiEffects),
+    ],
+  },
+];
