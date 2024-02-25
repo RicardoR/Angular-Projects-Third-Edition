b494cfb0032aeb0a474dde601647ac6be4825d2a
Ricardo <ricardo.rv@outlook.com>
2024-02-25 13:05:10 +0100

    feat: add getAll poi

--- Chapter08/libs/poi/src/lib/poi.service.ts
+++ Chapter08/libs/poi/src/lib/poi.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PoiEntity } from '../state/poi.models';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoiService {
+  private http = inject(HttpClient);
+
   getAll(): Observable<PoiEntity[]> {
-    throw new Error('Method not implemented.');
+    return this.http.get<PoiEntity[]>('assets/poi.json');
   }
 }
