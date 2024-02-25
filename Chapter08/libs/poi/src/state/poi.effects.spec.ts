638d7a6efab8ec3de11091b92a1456caeebbefe9
Ricardo <ricardo.rv@outlook.com>
2024-02-25 13:16:35 +0100

    test: poi effect

--- Chapter08/libs/poi/src/state/poi.effects.spec.ts
+++ Chapter08/libs/poi/src/state/poi.effects.spec.ts
@@ -1,28 +1,38 @@
+import { poiListMock } from './../lib/mocks/poi-list.mock';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Action } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 import { hot } from 'jasmine-marbles';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import * as PoiActions from './poi.actions';
 import { PoiEffects } from './poi.effects';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PoiService } from '../lib/poi.service';
 
 describe('PoiEffects', () => {
+  let poiServiceMock: { getAll: jest.Mock };
+
   let actions: Observable<Action>;
   let effects: PoiEffects;
 
   beforeEach(() => {
+    poiServiceMock = {
+      getAll: jest.fn()
+    };
     TestBed.configureTestingModule({
-      imports: [],
+      imports: [HttpClientTestingModule],
       providers: [
         PoiEffects,
         provideMockActions(() => actions),
         provideMockStore(),
+        { provide: PoiService, useValue: poiServiceMock },
       ],
     });
 
     effects = TestBed.inject(PoiEffects);
+    poiServiceMock.getAll.mockReturnValue(of(poiListMock));
   });
 
   describe('init$', () => {
@@ -30,7 +40,7 @@ describe('PoiEffects', () => {
       actions = hot('-a-|', { a: PoiActions.initPoi() });
 
       const expected = hot('-a-|', {
-        a: PoiActions.loadPoiSuccess({ poi: [] }),
+        a: PoiActions.loadPoiSuccess({ poi: poiListMock }),
       });
 
       expect(effects.init$).toBeObservable(expected);
