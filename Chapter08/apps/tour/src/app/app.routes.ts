c5939c835b7fd2c70929dffa8002a441f29744c3
Ricardo <ricardo.rv@outlook.com>
2024-02-25 11:56:12 +0100

    feat: add admin path

--- Chapter08/apps/tour/src/app/app.routes.ts
+++ Chapter08/apps/tour/src/app/app.routes.ts
@@ -1,11 +1,13 @@
-import { VisitorComponent } from '@packt/visitor';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
     path: 'tour',
-    loadComponent: () =>
-      import('@packt/visitor').then((c) => c.VisitorComponent),
+    loadComponent: () => import('@packt/visitor').then((c) => c.VisitorComponent),
+  },
+  {
+    path: 'admin',
+    loadComponent: () => import('@packt/admin').then(c => c.AdminComponent)
   },
   {
     path: '',
