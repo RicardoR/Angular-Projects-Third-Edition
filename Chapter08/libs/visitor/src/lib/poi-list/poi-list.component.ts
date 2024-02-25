5ffc5676a39dc72f7d1c1bca32832a7c7dfad2f4
Ricardo <ricardo.rv@outlook.com>
2024-02-25 13:45:52 +0100

    feat: add poi list in the poi list component

--- Chapter08/libs/visitor/src/lib/poi-list/poi-list.component.ts
+++ Chapter08/libs/visitor/src/lib/poi-list/poi-list.component.ts
@@ -1,16 +1,19 @@
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Component, OnInit, inject } from '@angular/core';
+import { MatActionList } from '@angular/material/list';
 import { Store } from '@ngrx/store';
-import { PoiActions } from '@packt/poi';
+import { PoiActions, PoiSelectors } from '@packt/poi';
 
 @Component({
   selector: 'packt-poi-list',
   standalone: true,
-  imports: [],
+  imports: [MatActionList],
   templateUrl: './poi-list.component.html',
-  styleUrl: './poi-list.component.css'
+  styleUrl: './poi-list.component.css',
 })
 export class PoiListComponent implements OnInit {
   private store = inject(Store);
+  pois = toSignal(this.store.select(PoiSelectors.selectAllPoi));
 
   ngOnInit(): void {
     this.store.dispatch(PoiActions.initPoi());
