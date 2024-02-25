ccedb40a26480c7e4f58c00ccadd05b223ef73a1
Ricardo <ricardo.rv@outlook.com>
2024-02-25 14:03:52 +0100

    fix: export poiEffects

--- Chapter08/libs/poi/src/index.ts
+++ Chapter08/libs/poi/src/index.ts
@@ -3,6 +3,6 @@ export * from './lib/poi/poi.component';
 import * as PoiActions from './state/poi.actions';
 import * as PoiFeature from './state/poi.reducer';
 import * as PoiSelectors from './state/poi.selectors';
-import { PoiEffects } from './state/poi.effects';
+export  { PoiEffects } from './state/poi.effects';
 
-export { PoiActions, PoiFeature, PoiSelectors, PoiEffects };
+export { PoiActions, PoiFeature, PoiSelectors };
