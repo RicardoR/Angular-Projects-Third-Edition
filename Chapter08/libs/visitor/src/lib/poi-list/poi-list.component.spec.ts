6d060fb0033f4aff73eb472a8086c41652d7b5ac
Ricardo <ricardo.rv@outlook.com>
2024-02-25 17:34:13 +0100

    ut: fix ut

--- Chapter08/libs/visitor/src/lib/poi-list/poi-list.component.spec.ts
+++ Chapter08/libs/visitor/src/lib/poi-list/poi-list.component.spec.ts
@@ -1,19 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PoiListComponent } from './poi-list.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { Store } from '@ngrx/store';
+import { PoiSelectors } from '@packt/poi';
 
 describe('PoiListComponent', () => {
+  const initialState = {
+
+  };
+
   let component: PoiListComponent;
   let fixture: ComponentFixture<PoiListComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PoiListComponent]
-    })
-    .compileComponents();
-    
+      imports: [PoiListComponent],
+      providers: [
+        provideMockStore({
+          selectors: [
+            { selector: PoiSelectors.selectAllPoi, value: [] },
+          ],
+        }),
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PoiListComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
--- Chapter08/libs/visitor/src/lib/visitor/visitor.component.spec.ts
+++ Chapter08/libs/visitor/src/lib/visitor/visitor.component.spec.ts
@@ -1,21 +1,30 @@
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { VisitorComponent } from './visitor.component';
+import { PoiSelectors } from '@packt/poi';
 
 describe('VisitorComponent', () => {
   let component: VisitorComponent;
   let fixture: ComponentFixture<VisitorComponent>;
+  let store: MockStore;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule]
+      imports: [NoopAnimationsModule],
+      providers: [
+        provideMockStore({
+          selectors: [{ selector: PoiSelectors.selectAllPoi, value: [] }],
+        }),
+      ],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VisitorComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
