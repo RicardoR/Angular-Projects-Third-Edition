fc13a36e0e7e8b31aced21a855e411e760493de5
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-02-26 19:54:46 +0100

    feat: render the card list inside of app component

--- Chapter09/src/app/app.component.spec.ts
+++ Chapter09/src/app/app.component.spec.ts
@@ -1,21 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { assassins } from './assassins';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+  
+  it('should contains a cardsList', () => {
+    expect(app.cards()).toEqual(assassins);
   });
 
   it('should render the cards', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('lib-card-list')).toBeTruthy();
