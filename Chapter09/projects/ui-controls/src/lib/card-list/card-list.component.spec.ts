b57ddaf60374cae712de8c38c495012bbc59f59e
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-02-27 09:12:23 +0100

    ut: add test for sorting event

--- Chapter09/projects/ui-controls/src/lib/card-list/card-list.component.spec.ts
+++ Chapter09/projects/ui-controls/src/lib/card-list/card-list.component.spec.ts
@@ -2,11 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardListComponent } from './card-list.component';
 import { cardsListMock } from '../test/cards-list.mock';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 
 describe('CardListComponent', () => {
   let component: CardListComponent;
   let fixture: ComponentFixture<CardListComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CardListComponent]
@@ -26,5 +26,13 @@ describe('CardListComponent', () => {
   it('should render the cards', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelectorAll('[data-test=card]').length).toBe(cardsListMock.length);
+  });
+
+  it('should emit the new card position when sortCads is called', () => {
+    const emitSpy = spyOn(component.cardChange, 'emit');
+    const cardList = ['Card 2', 'Card 3', 'Card 1'];
+    const myEvent = cardList as unknown as CdkDragDrop<string[]>;
+    component.sortCards(myEvent);
+    expect(emitSpy).toHaveBeenCalledWith(component.cards());
   });
 });
