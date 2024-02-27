d0d222d624c3dcf23f60963cc553339ebc0af47c
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-02-27 09:13:01 +0100

    feat: sort data

--- Chapter09/projects/ui-controls/src/lib/card-list/card-list.component.ts
+++ Chapter09/projects/ui-controls/src/lib/card-list/card-list.component.ts
@@ -1,13 +1,23 @@
-import { Component, input } from '@angular/core';
+import { Component, EventEmitter, input, Output } from '@angular/core';
 import { Card } from '../domain/card';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'lib-card-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CdkDropList,
+    CdkDrag
+  ],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.css'
 })
 export class CardListComponent {
   cards = input<Card[]>([]);
+  @Output() cardChange = new EventEmitter<Card[]>();
+
+  sortCards(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.cards(), event.previousIndex, event.currentIndex);
+    this.cardChange.emit(this.cards());
+  }
 }
