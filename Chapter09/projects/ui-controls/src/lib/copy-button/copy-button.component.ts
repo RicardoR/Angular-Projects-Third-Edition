ace3347c05b4e68ea11c0638087794169015b745
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-02-27 19:22:30 +0100

    feat: add copy to clipboard

--- Chapter09/projects/ui-controls/src/lib/copy-button/copy-button.component.ts
+++ Chapter09/projects/ui-controls/src/lib/copy-button/copy-button.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'lib-copy-button',
   standalone: true,
-  imports: [],
+  imports: [
+    CdkCopyToClipboard
+  ],
   templateUrl: './copy-button.component.html',
   styleUrl: './copy-button.component.css'
 })
 export class CopyButtonComponent {
-
+  data = model<string>('');
+  
+  onCopy(): void {
+    this.data.set('')
+  }
 }
--- Chapter09/src/app/app.component.ts
+++ Chapter09/src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, Signal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Card, CardListComponent } from 'ui-controls';
+import { Card, CardListComponent, CopyButtonComponent } from 'ui-controls';
 import { assassins } from './assassins';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardListComponent],
+  imports: [RouterOutlet, CardListComponent, FormsModule, CopyButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   cards = signal<Card[]>(assassins);
-
+  title: string = '';
+  
   onChange() {
     console.log(this.cards());
+  }
+  
+  log() {
+    alert(this.title + ' copied to the clipboard');
   }
 }
