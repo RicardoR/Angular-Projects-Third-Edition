ea56c61425afa60acf989c5166519746a284e4ac
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-02-27 19:12:20 +0100

    feat: add copy button component

---
+++ Chapter09/projects/ui-controls/src/lib/copy-button/copy-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-copy-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './copy-button.component.html',
+  styleUrl: './copy-button.component.css'
+})
+export class CopyButtonComponent {
+
+}
--- Chapter09/projects/ui-controls/src/public-api.ts
+++ Chapter09/projects/ui-controls/src/public-api.ts
@@ -5,4 +5,5 @@
 export * from './lib/ui-controls.service';
 export * from './lib/ui-controls.component';
 export * from './lib/card-list/card-list.component';
+export * from './lib/copy-button/copy-button.component';
 export * from './lib/domain/card';
